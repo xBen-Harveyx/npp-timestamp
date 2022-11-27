4ea30ba380ff0170b927295b8ea95090a23194e5
Ben Harvey <115814831+xBen-Harveyx@users.noreply.github.com>
2022-11-27 23:05:56 +0000

    added the start and stop scripts.

---
+++ time-start.py
@@ -0,0 +1,5 @@
+import time
+# import sys
+timeStr = time.strftime('START'  '--'  '%A -- %B %d %Y' + ' @ ' + '%X %p' )
+# sys.stdout.write(timeStr)
+editor.addText( timeStr )
---
+++ time-stop.py
@@ -0,0 +1,5 @@
+import time
+# import sys
+timeStr = time.strftime('*END*' '%A -- %B %d %Y' + ' @ ' + '%X %p' )
+# sys.stdout.write(timeStr)
+editor.addText( timeStr )
